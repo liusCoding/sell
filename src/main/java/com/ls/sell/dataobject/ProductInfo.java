de0dfdc3cb435edd5de076edf584cbab406582e1
liusCoding <14786469221@163.com>
2019-10-27 21:20:26 +0800

    修改注释，添加项目文档

--- src/main/java/com/ls/sell/dataobject/ProductInfo.java
+++ src/main/java/com/ls/sell/dataobject/ProductInfo.java
@@ -15,29 +15,29 @@ import java.math.BigDecimal;
 @Entity
 @Data
 public class ProductInfo {
-    //商品ID
+    /** 商品ID */
     @Id
     private String productId;
 
-    //商品名字
+    /** 商品名字 */
     private String productName;
 
-    //商品单价
+    /** 商品单价 */
     private BigDecimal productPrice;
 
-    //商品库存
+    /** 商品库存 */
     private Integer productStock;
 
-    //商品描述
+    /** 商品描述 */
     private String productDescription;
 
-    //商品小图
+    /** 商品小图 */
     private String productIcon;
 
-    //商品的状态  0正常  1下架
+    /** 商品的状态  0正常  1下架*/
     private Integer productStatus;
 
-    //类目编号
+    /** 类目编号*/
     private Integer categoryType;
 
 
